6db6e572b51d09d143b6b73b78f4e5c3a67e0579
chinghuashih <stewardshih@gmail.com>
2021-12-08 10:52:35 -0500

    Create spikeIn.nucleosome.sh

---
+++ spikeIn.nucleosome.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#SBATCH -p standard -o CnT.nucleosomeSpikeIn.counts.log -t 4:00:00
+#SBATCH -c 2 --mem=32G
+
+# Notes:
+# EpiCypher considers an antibody with <20% binding to all off-target PTMs specific and suitable for downstream data analysis. 
+# For IgG, data is normalized to the sum of total barcode reads.
+
+sampleFiles=$(<samples.txt)
+
+echo -e \
+	sample"\t"\
+	raw_counts"\t"\
+	barcode"\t"\
+	R1.count"\t"\
+	R2.count > spikeIn.counts.txt
+
+for sample in ${sampleFiles[*]}
+do
+	echo "${sample}"
+
+	counts=$(zcat ${sample}.R1.fastq.gz | wc -l)
+	counts=$(echo ${counts}"/4" | bc -l)
+
+	for barcode in \
+		TTCGCGCGTAACGACGTACCGT CGCGATACGACCGCGTTACGCG \
+		CGACGTTAACGCGTTTCGTACG CGCGACTATCGCGCGTAACGCG \
+		CCGTACGTCGTGTCGAACGACG CGATACGCGTTGGTACGCGTAA \
+		TAGTTCGCGACACCGTTCGTCG TCGACGCGTAAACGGTACGTCG \
+		TTATCGCGTCGCGACGGACGTA CGATCGTACGATAGCGTACCGA \
+		CGCATATCGCGTCGTACGACCG ACGTTCGACCGCGGTCGTACGA \
+		ACGATTCGACGATCGTCGACGA CGATAGTCGCGTCGCACGATCG \
+		CGCCGATTACGTGTCGCGCGTA ATCGTACCGCGCGTATCGGTCG \
+		CGTTCGAACGTTCGTCGACGAT TCGCGATTACGATGTCGCGCGA \
+		ACGCGAATCGTCGACGCGTATA CGCGATATCACTCGACGCGATA \
+		CGCGAAATTCGTATACGCGTCG CGCGATCGGTATCGGTACGCGC \
+		GTGATATCGCGTTAACGTCGCG TATCGCGCGAAACGACCGTTCG \
+		CCGCGCGTAATGCGCGACGTTA CCGCGATACGACTCGTTCGTCG \
+		GTCGCGAACTATCGTCGATTCG CCGCGCGTATAGTCCGAGCGTA \
+		CGATACGCCGATCGATCGTCGG CCGCGCGATAAGACGCGTAACG \
+		CGATTCGACGGTCGCGACCGTA TTTCGACGCGTCGATTCGGCGA ;
+	do
+		R1count=$(zcat ${sample}.R1.fastq.gz | grep -c ${barcode})
+		R2count=$(zcat ${sample}.R2.fastq.gz | grep -c ${barcode})
+		echo -e \
+			${sample}"\t"\
+			${counts}"\t"\
+			${barcode}"\t"\
+			${R1count}"\t"\
+			${R2count} >> spikeIn.counts.txt
+	done
+done
+
+
+
+
+
+
+
+
+
